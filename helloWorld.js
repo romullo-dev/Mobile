c56e0299f9892f8a96516ed816c1fcffb6372c0f
Rômulo França <157732149+romullo-dev@users.noreply.github.com>
2025-04-28 21:42:27 -0300

    Commit

---
+++ helloWorld.js
@@ -0,0 +1,2 @@
+console.log('Hello World');
+document.writeln('Hello World');
---
+++ variaveias.js
@@ -0,0 +1,11 @@
+let nome;
+
+let cpf = "01235212430";
+
+nome ="Maria";
+
+const dolar=5.65;
+
+console.log(nome);
+
+document.writeln(cpf)
